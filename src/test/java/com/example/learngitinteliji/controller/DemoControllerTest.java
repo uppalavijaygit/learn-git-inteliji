1e208e81f32e9dd0dd8320d6c63e42b1564aac84
vijay kumar uppala <vijay.kumaruppala@infotel.com>
2022-04-19 07:35:41 +0100

    # Conflicts:

--- src/test/java/com/example/learngitinteliji/controller/DemoControllerTest.java
+++ src/test/java/com/example/learngitinteliji/controller/DemoControllerTest.java
@@ -3,19 +3,9 @@ package com.example.learngitinteliji.controller;
 import static org.junit.jupiter.api.Assertions.*;
 
 class DemoControllerTest {
-    // API Writing start
+    public static void main(String[] args) {
 
-    // API Development End
-
-    //CHAG 1
-
-    //CHAG 4
-
-    // CHANGE 5
-
-    // CHANGE 6
-
-    //ADD IN MASTER
+    }
 
 
 }
