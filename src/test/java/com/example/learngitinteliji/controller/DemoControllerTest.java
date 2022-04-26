0edb59e541a776414fd76d670b7d294a3011011e
vijay kumar uppala <vijay.kumaruppala@infotel.com>
2022-04-26 18:31:41 +0100

    COMMENT 1 AND 2 AND 3

--- src/test/java/com/example/learngitinteliji/controller/DemoControllerTest.java
+++ src/test/java/com/example/learngitinteliji/controller/DemoControllerTest.java
@@ -1,9 +1,13 @@
 package com.example.learngitinteliji.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DemoControllerTest {
     public static void main(String[] args) {
+        //COMMENT 1
+
+        //COMMENT 2
+
+        //ADDED COMMENT 3
+        System.out.print("Hello");
 
     }
 
